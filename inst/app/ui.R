0275c78f495919d45e5dbab650bf5544df88ff40
Sean Tang <seantang001>
2021-01-04 21:47:04 -0800

    tried to fix scenario auto select bug

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -70,9 +70,8 @@ ui <- fluidPage(
   div(actionLink(inputId='help', label='', icon = icon("question","fa-1x"),
                  onclick ="window.open('https://jgcri.github.io/rdataviz/', '_blank')"),style="padding:15px 5px;float: right"),
   # div(style = "float:left;",fas fa-cog"
-    div(
-      tags$a(id = "downloadSettings", class = "btn btn-default shiny-download-link download_button", href = "", target = "_blank", download = NA, icon("cog"), "Save Settings"), style="padding:5px; float:left"),
-    div(style = "padding:5px", tags$label(class="btn btn-default shiny-download-link download_button shiny-bound-output", tags$span(class="btn download_button", "Upload Settings", style = "padding:0px", tags$i(class="fas fa-cog", style="float:left;margin-right:5px"), tags$input(type="file", id="settingdata", name="settingdata", class="shiny-bound-input", style = "display:none")))),
+    div(tags$a(id = "downloadSettings", class = "btn btn-default shiny-download-link download_button", href = "", target = "_blank", download = NA, icon("cog"), "Save Settings"), style="padding:5px; float:left;margin-top:5px"),
+    div(style = "padding:5px;margin-top:5px", tags$label(class="btn btn-default shiny-download-link download_button shiny-bound-output", tags$span(class="btn download_button", "Upload Settings", style = "padding:0px", tags$i(class="fas fa-cog", style="float:left;margin-right:5px"), tags$input(type="file", id="settingdata", name="settingdata", class="shiny-bound-input", style = "display:none")))),
     # ),
 
 
